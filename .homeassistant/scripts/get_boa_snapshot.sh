e4a81a69bc96ae4cb0bb3cba28fd297858a75316
Yakov Borisov <yakov_borisov@mail.ru>
2025-07-11 21:50:07 +0300

    zigbee

--- .homeassistant/scripts/get_boa_snapshot.sh
+++ .homeassistant/scripts/get_boa_snapshot.sh
@@ -7,3 +7,5 @@ if [ $RETVAL -eq 52 ]
 ffmpeg -fflags discardcorrupt -t 30 -rtsp_transport tcp -i rtsp://admin:admin@192.168.20.110:554/1/h264major/multicast -y -frames:v 1 -f image2 /config/www/boa_snapshot.jpg > /tmp/ffmpeg_out.txt 2>/tmp/ffmpeg_err.txt
 fi
 
+
+curl -s --anyauth -u homeassistant:a6xzoqz_Z -o /config/www/hik_snapshot.jpg http://192.168.20.85/onvif-http/snapshot?Profile_1
